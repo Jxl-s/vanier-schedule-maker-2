e44f0d038949b452540571a91b656609e90dc489
j <77560326+Jxl-s@users.noreply.github.com>
2024-08-09 12:02:14 -0400

    update dump

--- dump.js
+++ dump.js
@@ -1,127 +1,95 @@
+console.error = () => {};
+console.warn = () => {};
+
+const allData = {};
 (async () => {
-    console.error = () => {};
-    console.warn = () => {};
-    const customLog = (...text) => console.log("[Dumper]", ...text);
+    for (let i = 0; i < 50; i++) {
+        const btns = [...document.getElementsByClassName("details-link has-tooltip launch-modal")];
+        const iframe = document.querySelector("iframe[aria-hidden=true]");
 
-    const allData = {};
+        const waitUntilLoad = () => {
+            return new Promise((resolve, reject) => {
+                let interval = setInterval(() => {
+                    console.log(iframe.contentDocument
+                        .querySelector(".view-loading .message .text-center"));
+                    if (
+                        iframe.contentDocument.querySelectorAll("[data-entity=vit_meetingtime")
+                            .length > 0 &&
+                        iframe.contentDocument
+                            .querySelector(".view-loading")
+                            ?.getAttribute("style") === "display: none;"
+                    ) {
+                        clearInterval(interval);
+                        return resolve();
+                    }
 
-    const INTERESTED_COURSES = [""];
+                    console.log("Waiting for iframe to load...");
+                }, 1000);
+            });
+        };
 
-    // helpers
-    const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-    const sleepUntilExist = async (selector) => {
-        while (!document.querySelector(selector)) {
-            await sleep(500);
-        }
-    };
+        // Press on the button
+        for (const btn of btns) {
+            btn.click();
+            await waitUntilLoad();
 
-    // inputs
-    const searchBox = document.querySelector("input.query.form-control");
-    const searchBtn = document.querySelector("button[title='Search Results']");
+            const doc = iframe.contentDocument;
 
-    const searchCourse = async (course) => {
-        customLog(`Searching for ${course}`);
-        searchBox.value = course;
-        await sleep(500);
+            const courseName = doc.getElementById("vit_coursetitle").getAttribute("value");
+            const courseCode = doc.getElementById("vit_course").getAttribute("value");
+            // const section = doc.getElementById("vit_sec").getAttribute("value");
+            const section = btn.innerText.trim();
 
-        searchBtn.click();
-        await sleep(1000);
-        await sleepUntilExist("table.table.table-striped > tbody");
-    };
-
-    async function dumpPages() {
-        for (let i = 0; i < 58; i++) {
-            const btns = [
-                ...document.getElementsByClassName("details-link has-tooltip launch-modal"),
-            ];
-            const iframe = document.querySelector("iframe[aria-hidden=true]");
-
-            const waitUntilLoad = () => {
-                return new Promise((resolve, reject) => {
-                    let interval = setInterval(() => {
-                        if (
-                            iframe.contentDocument.querySelectorAll("[data-entity=vit_meetingtime")
-                                .length > 0
-                        ) {
-                            clearInterval(interval);
-                            return resolve();
-                        }
-
-                        customLog("Waiting for iframe to load...");
-                    }, 2000);
-                });
-            };
-
-            // Press on the button
-            for (const btn of btns) {
-                btn.click();
-                await waitUntilLoad();
-
-                const doc = iframe.contentDocument;
-
-                const courseName = doc.getElementById("vit_coursetitle").getAttribute("value");
-                const courseCode = doc.getElementById("vit_course").getAttribute("value");
-                const section = doc.getElementById("vit_sec").getAttribute("value");
-
-                // If it's a new department (3 first letters), add a key in the object
-                const department = courseCode.slice(0, 3);
-                if (!allData[department]) {
-                    allData[department] = [];
-                }
-
-                const classesArr = [];
-                // example data: {"101":[{"section":"00001","courseId":"101-101-VA","title":"Anatomy and Physiology I","classes":[{"day":"Mon","time":"8:00 - 10:00","room":"A-320","teacher":"Panzuto, Maria T."},{"day":"Tue","time":"10:00 - 13:00","room":"A-453","teacher":"Chit, Fallah Hassan"},{"day":"Fri","time":"8:00 - 10:00","room":"C-418","teacher":"Panzuto, Maria T."}]},{"section":"00002","courseId":"101-101-VA","title":"Anatomy and Physiology I","classes":[{"day":"Mon","time":"8:00 - 10:00","room":"A-320","teacher":"Panzuto, Maria T."},{"day":"Tue","time":"13:00 - 16:00","room":"A-453","teacher":"Chit, Fallah Hassan"}
-                allData[department].push({
-                    section,
-                    courseId: courseCode,
-                    title: courseName,
-                    classes: classesArr,
-                });
-
-                const meetings = [...doc.querySelectorAll("[data-entity=vit_meetingtime")];
-                customLog(`${courseCode} ${courseName} ${meetings.length}`);
-
-                for (const meeting of meetings) {
-                    const teacher = meeting.querySelector("[data-attribute=vit_teacher]").innerText;
-                    const day = meeting
-                        .querySelector("[data-attribute=vit_day]")
-                        .innerText.replace(/\./g, "");
-                    const time = meeting.querySelector("[data-attribute=vit_time]").innerText;
-                    const room = meeting.querySelector("[data-attribute=vit_room]").innerText;
-
-                    classesArr.push({
-                        teacher,
-                        day,
-                        time,
-                        room,
-                    });
-                }
+            // If it's a new department (3 first letters), add a key in the object
+            const department = courseCode.slice(0, 3);
+            if (!allData[department]) {
+                allData[department] = [];
             }
 
-            customLog("Current output:\n", JSON.stringify(allData));
-            const nextButton = document.querySelector("[aria-label='Next page']");
-            if (
-                nextButton?.checkVisibility() &&
-                (!nextButton?.getAttribute("aria-disabled") ?? false)
-            ) {
-                nextButton.click();
+            const classesArr = [];
+            // example data: {"101":[{"section":"00001","courseId":"101-101-VA","title":"Anatomy and Physiology I","classes":[{"day":"Mon","time":"8:00 - 10:00","room":"A-320","teacher":"Panzuto, Maria T."},{"day":"Tue","time":"10:00 - 13:00","room":"A-453","teacher":"Chit, Fallah Hassan"},{"day":"Fri","time":"8:00 - 10:00","room":"C-418","teacher":"Panzuto, Maria T."}]},{"section":"00002","courseId":"101-101-VA","title":"Anatomy and Physiology I","classes":[{"day":"Mon","time":"8:00 - 10:00","room":"A-320","teacher":"Panzuto, Maria T."},{"day":"Tue","time":"13:00 - 16:00","room":"A-453","teacher":"Chit, Fallah Hassan"}
+            allData[department].push({
+                section,
+                courseId: courseCode,
+                title: courseName,
+                classes: classesArr,
+            });
 
-                await new Promise((resolve, reject) => setTimeout(resolve, 4000));
-                customLog(
-                    `Currently on page ${
-                        document.querySelector(".active > [aria-current=page]")?.innerText ?? "?"
-                    }`
-                );
-            } else {
-                break;
+            const meetings = [...doc.querySelectorAll("[data-entity=vit_meetingtime")];
+            console.log(`${courseCode} ${courseName} ${meetings.length}`);
+
+            for (const meeting of meetings) {
+                const teacher = meeting.querySelector("[data-attribute=vit_teacher]")?.innerText;
+                const day = meeting.querySelector("[data-attribute=vit_day]")?.innerText;
+                const time = meeting.querySelector("[data-attribute=vit_time]")?.innerText;
+
+                if (!teacher || !day || !time) {
+                    continue
+                }
+
+                classesArr.push({
+                    teacher,
+                    day,
+                    time,
+                });
             }
         }
 
-        customLog("Current output:\n", JSON.stringify(allData));
-    }
+        const curPage = document.querySelector(".active > [aria-current=page]")?.innerText ?? "?";
+        if (curPage.toString() == "34") {
+            console.log(JSON.stringify(allData));
+            break;
+        }
 
-    for (const course of INTERESTED_COURSES) {
-        await searchCourse(course);
-        await dumpPages();
+        document.querySelector("[aria-label='Next page']").click();
+
+        await new Promise((resolve, reject) => setTimeout(resolve, 4000));
+        console.log(
+            `Currently on page ${
+                document.querySelector(".active > [aria-current=page]")?.innerText ?? "?"
+            }`
+        );
+
+        console.log(JSON.stringify(allData));
     }
 })();
